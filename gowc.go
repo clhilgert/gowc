cecbc054406b35eb8a820c90f20fd525b344b326
Clay Hilgert <claytonhilgertmst@gmail.com>
2024-11-22 17:46:49 -0600

    count bytes

--- gowc.go
+++ gowc.go
@@ -1,11 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println(testMain())
+	bytesFlag := flag.Bool("c", false, "Enable -c flag")
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) == 0 {
+		fmt.Println("No filename provided")
+		return
+	}
+	filename := args[0]
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	if *bytesFlag {
+		countBytes(file, filename)
+	}
+
 }
 
-func testMain() string {
-	return "test"
+func countBytes(file *os.File, filename string) {
+	buffer := make([]byte, 1024)
+	totalBytes := int64(0)
+
+	for {
+		n, err := file.Read(buffer)
+		if err != nil && err.Error() != "EOF" {
+			fmt.Println("Error reading file:", err)
+			return
+		}
+		totalBytes += int64(n)
+
+		if err != nil {
+			break
+		}
+	}
+	fmt.Println(totalBytes, filename)
 }
